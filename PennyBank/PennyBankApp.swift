8899d945b61667523ebd4fbcd8e079de4c3ae8e9
guavacadodev <jakewoodall001@gmail.com>
2025-12-27 18:35:26 -0800

    Initial Commit

---
+++ PennyBank/PennyBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyBankApp.swift
+//  PennyBank
+//
+//  Created by Jake Woodall on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct PennyBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PennyBankTests/PennyBankTests.swift
@@ -0,0 +1,17 @@
+//
+//  PennyBankTests.swift
+//  PennyBankTests
+//
+//  Created by Jake Woodall on 12/27/25.
+//
+
+import Testing
+@testable import PennyBank
+
+struct PennyBankTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
